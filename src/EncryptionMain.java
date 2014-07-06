c4e895f07ff6ead53bb689e0503b87b6bc58524f
Joerg Wegener <joerg.wegener@gmail.com>
2014-07-06 12:46:24 +0200

    Zwischenstand, noch nicht 100% durchgetestet

--- src/EncryptionMain.java
+++ src/EncryptionMain.java
@@ -1,12 +1,163 @@
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
 public class EncryptionMain {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		String passphrase  = null;
+		String cleartext   = null;
+		String cryptotext  = null;
+		
+		boolean isPassphraseOK = false;
+		
+		
+		// Main process: read a clear text, encrypt it, return it.
+		// TODO: Branch, so that the method can both encrypt and decrypt!
+		// TODO: In order to do so, this functionality needs to be encapsulated, and
+		// TODO: the main method must query the intend of the caller.
+		
+		do {
+			passphrase = getPassphrase();
+			isPassphraseOK = checkPassphrase( passphrase );	
+		} while ( !isPassphraseOK );
+				
+		cleartext = getCleartext();
+		
+		cryptotext = encryptString( passphrase, cleartext );
+		
+		printCryptotext( cryptotext );
 
 	}
+	
+	
+	// Read a string from the console. NOTE: This could be
+	// changed in a later version to allow a GUI input.
+	private static String getPassphrase () {
+		
+		boolean isStringOk = false;
+		String s = "";
+		
+		System.out.println( "Bitte das Kennwort eingeben:\n" );
+		do {
+
+			try {
+				BufferedReader bufferread = new BufferedReader( new InputStreamReader( System.in ) );
+				s = bufferread.readLine().toString();
+			}
+			catch ( IOException e ) {
+				e.printStackTrace();
+			}
+			
+			isStringOk = checkStringContent(s);
+			
+		} while ( !isStringOk );
+
+		// Remove spaces and convert to upper case
+		s = s.toUpperCase();
+		s.replaceAll( " ", "" );
+		
+		return s;
+	}
+	
+	
+	// Read input text; check that only characters, space, and numbers are used.
+	// Repeat until input is correct.
+	private static String getCleartext () {
+		
+		boolean isStringOk = false;
+		String s = "";
+		
+		System.out.println( "Bitte den Klartext eingeben:\n" );
+		
+		do {
+
+			try {
+				BufferedReader bufferread = new BufferedReader( new InputStreamReader( System.in ) );
+				s = bufferread.readLine().toString();
+			}
+			catch ( IOException e ) {
+				e.printStackTrace();
+			}
+			
+			isStringOk = checkStringContent( s );
+			
+		} while ( !isStringOk );
+		
+		// Replace ß before conversion (otherwise it will not work)
+		s.replaceAll( "ß", "SS" );
+		
+		// Convert to upper case and remove spaces
+		s = s.toUpperCase();
+		s.replaceAll( " ", "" );
+		
+		// Now convert the rest of the umlauts
+		s.replaceAll( "Ä", "AE" );
+		s.replaceAll( "Ö", "OE" );
+		s.replaceAll( "Ü", "UE" );
+		
+		return s;
+	}
+	
+	
+	// Check the passphrase. The only rule in our algorithm is
+	// that no character may appear more than once.
+	private static boolean checkPassphrase( String passphrase ) {
+		
+		for ( int i = 0; i < passphrase.length()-1; i++ ) {
+			for ( int j = i+1; j < passphrase.length(); j++ ) {
+				if ( passphrase.charAt( i ) == passphrase.charAt( j ) ) {
+					System.out.println( "Das Kennwort muss aus unterschiedlichen Zeichen bestehen.\n" );
+					return false;
+				}
+			}
+		}
+		
+		return true;		
+	}
+	
+	
+	// Encryption functionality
+	private static String encryptString( String passphrase, String clearText ) {
+        EncryptionMatrix myMatrix = new EncryptionMatrix( passphrase );
+		
+		// DEBUG
+        myMatrix.printMatrix();
+
+		return myMatrix.encryptText( clearText );
+	}
+	
+	
+	// Output. Note that this could be changed to provide the output in a GUI.
+	private static void printCryptotext( String cryptotext ) {
+		System.out.println( "Verschlüsselter Text:\n" );
+		System.out.println( cryptotext );
+		System.out.println( "Und jetzt in 5er Gruppen:\n" );
+		while ( cryptotext.length() >= 5 ){
+			System.out.print( cryptotext.substring( 0, 5 ) + " ");
+			cryptotext = cryptotext.substring( 5 );
+		}
+		System.out.println( cryptotext );
+	}
+
+    
+	// Only characters, numbers and space allowed in passphrases and cleartexts.
+	// Check this and provide a hint to the user if violated.
+	private static boolean checkStringContent( String str ) {
+        if ( str == null ) {
+        	System.out.println( "Bitte mindestens 1 Zeichen oder Zahl eingeben!\n" );
+            return false;
+        }
+        for ( int i = 0; i < str.length(); i++ ) {
+            if ( ( Character.isLetterOrDigit( str.charAt( i ) ) == false ) && ( str.charAt( i ) != ' ') ) {
+            	System.out.println( "Bitte lediglich Zeichen, Zahlen und Leerzeichen eingeben! Umlaute sind nicht zulässig\n" );
+                return false;
+            }
+        }
+        return true;
+    }    
 
 }
--- src/EncryptionMatrix.java
+++ src/EncryptionMatrix.java
@@ -1,4 +1,149 @@
 
 public class EncryptionMatrix {
+	
+	
+	private final int MATRIXDIM = 6; // I hate magic numbers, always use constants
+	
+	
+	// The encryption matrix is a 6x6 matrix. It contains all characters
+	// and the numbers 0..9. SPACE is not needed, because it will always
+	// be translated into SPACE. Yes, this is a weakness, but hey, this
+	// encryption algorithm is not designed to withstand the NSA. It is
+	// meant for me to practice my Java, nothing more, nothing less.
+	private char [][] encryptionMatrix = new char [MATRIXDIM][MATRIXDIM];
+	
+	// Constructor
+	public EncryptionMatrix( String passphrase ) {
+		
+		String charsForMatrixInsertion = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"; // these still need to be inserted into the matrix
+		
+		// Write the passphrase into the beginning of the matrix
+	    for ( int count = 0; count < passphrase.length(); count++ ) {
+	    	// Determine the coordinates for the character in the 
+	    	// matrix: X is "count MOD 6" (width of the matrix),
+	    	// Y is "count DIV 6" (depth of the matrix).
+	    	int x = count % MATRIXDIM;
+	    	int y = count / MATRIXDIM;
+	    	
+	    	// Now we know the square, and we fill it
+	    	this.encryptionMatrix[x][y] = passphrase.charAt(count);
+	    	
+	    	// The character is already used, so we remove it from the string that
+	    	// contains the still-to-be-inserted characters:
+	    	for ( int i = 0; i < charsForMatrixInsertion.length(); i++ ) {
+	    		if ( charsForMatrixInsertion.charAt( i ) == passphrase.charAt( count ) ) {
+	    			charsForMatrixInsertion = charsForMatrixInsertion.substring( 0,  i ) + charsForMatrixInsertion.substring( i+1 );
+	    			break; // There is only ever one hit, so we can leave the loop here
+	    		}
+	    	}
+	    }
+	    
+	    // Now we add all the characters and numbers that were NOT part of 
+	    // the passphrase
+	    int count = passphrase.length();
+	    for ( char c : charsForMatrixInsertion.toCharArray() ) {
+	    	int x = count / MATRIXDIM;
+	    	int y = count % MATRIXDIM;
+	    	this.encryptionMatrix[x][y] = c;
+	    	count++;
+	    }
+	}
+
+	
+    // This is the main method. Needs proper documentation.
+	public String encryptText( String clearText ) {
+		
+		String cryptoText = ""; // This is the result
+		
+		// Loop over the input string. Build pairs of characters.
+		// Determine their position
+		for ( int i = 0; i < clearText.length() / 2; i++ ) {
+			char c1, c2;      // Characters to be compared
+			int x1, y1;       // Coordinates of the first character
+			int x2, y2;       // Coordinates of the second character
+			int temp1, temp2; // temps for the determination of the coordinates
+			
+			c1 = clearText.charAt( i * 2 );
+			c2 = clearText.charAt( ( i * 2 )+1 );
+			temp1 = this.findPosition( c1 );
+			temp2 = this.findPosition( c2 );
+			x1 = temp1 / 10; y1 = temp1 % 10;
+			x2 = temp2 / 10; y2 = temp2 % 10;
+		
+			// Now we have to look at the relative positions and 
+			// do the encryption.
+			cryptoText += this.encryptCharPair( x1, y1, x2, y2 );
+		
+		}
+		
+		// If there is an uneven number of chars, the last one is moved
+		// down one square
+		if ( ( clearText.length() % 2 ) == 1 ) {
+			char c;
+			int temp;
+			c = clearText.charAt( clearText.length() - 1 );
+			temp = this.findPosition( c );
+			cryptoText += this.encryptionMatrix[ temp / 10 ][ ( ( temp % 10 )+1 ) % MATRIXDIM ];
+		}
+		
+    	return cryptoText;
+    }
+	
+	
+	// Helper method that returns a string of 2 chars for the clear chars provided.
+	// This result has to be added to the encrypted string.
+	private String encryptCharPair ( int x1, int y1, int x2, int y2 ) {
+		String result = "";
+		
+		// Determine the relative position of the 2 characters
+		if ( ( x1 != x2 ) && ( y1 != y2 ) ) {
+			// Both axes are different. Swap horizontally
+			result += this.encryptionMatrix[ x2 ][ y1 ]; // Move c1 horizontally
+			result += this.encryptionMatrix[ x1 ][ y2 ]; // Move c2 horizontally
+		} else if ( ( x1 == x2 ) && ( y1 != y2) ) { 
+			// On the same X axis. Move both chars down one space; roll over if needed
+			result += this.encryptionMatrix[ x1 ][ (y1 + 1) % MATRIXDIM ];
+			result += this.encryptionMatrix[ x2 ][ (y2 + 1) % MATRIXDIM ];
+		} else if ( (x1 != x2 ) && ( y1 == y2 ) ) {
+			// On the same Y axis. Move right one space; roll over if needed
+			result += this.encryptionMatrix[ (x1 + 1) % MATRIXDIM ][ y1 ];
+			result += this.encryptionMatrix[ (x2 + 1) % MATRIXDIM ][ y2 ];
+		} else if ( ( x1 == x2 ) && ( y1 == y2 ) ) {
+			// Identical characters: move both one space down, roll over if needed
+			result += this.encryptionMatrix[ x1 ][ (y1 + 1) % MATRIXDIM ];
+			result += this.encryptionMatrix[ x2 ][ (y2 + 1) % MATRIXDIM ];
+		} else {
+			// SHIT this is not supposed to happen!
+			System.out.println( "Clusterfuck; this coding should be completely unreachable!\n" );
+		}
+		return result;
+	}
+	
+	
+	
+	// Helper method. The tens determine the X position, the Ones the Y position
+	// (How I HATE it that Java does not have structs, or export parameters!)
+	private int findPosition ( char c ) {
+		int result = 0;
+		mainloop: 
+		for ( int y = 0; y < MATRIXDIM; y++ )
+			for ( int x = 0; x < MATRIXDIM; x++ )
+				if ( this.encryptionMatrix[x][y] == c ) {
+					result = ( 10*x ) + y;
+					break mainloop; // Only one hit possible, so we can leave the loop
+				}
+		return result;
+	}
+    
+	// For testing purposes only!
+	public void printMatrix () {
+    	for ( int y = 0; y < MATRIXDIM; y++ ) {
+    		for ( int x = 0; x < MATRIXDIM; x++ ) {
+    			System.out.print( this.encryptionMatrix[x][y] );
+    		}
+    		System.out.println("\n");
+    	}
+		return;
+    }
 
 }
